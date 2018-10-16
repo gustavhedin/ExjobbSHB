d453d450864a07114bbf5e030275c35197a7da1f
gustavhedin <31615071+gustavhedin@users.noreply.github.com>
2018-10-16 10:06:27 +0200

    Updated VAD

--- VAD.m
+++ VAD.m
@@ -6,7 +6,7 @@ Startvalue = 10;    % Starting value for the underlying asset at time 0.
 T = 1;              % Time horizon
 N = 1000;           % # simulation points on [0,T];
 K = 10;             % Strike price
-nbr_MC = 10000;     % # of Monte Carlo simulations
+nbr_MC = 10000;         % # of Monte Carlo simulations
 nbrMC_z = 10;       % # of samples over the barrier
 
 % Declaring nedded variables:
@@ -37,20 +37,20 @@ end
  
  % Find price 
  Z = randn(1,nbr_MC);
- Xend=X(end,:)+r*h*X(end,:) + sigma*sqrt(h)*X(end,:).*Z;
- Price=mean(max(Xend-K,0))*exp(-r*T);   
+ Xend = X(end,:)+r*h*X(end,:) + sigma*sqrt(h)*X(end,:).*Z;
+ Price = mean(max(Xend-K,0))*exp(-r*T);   
  
  % Generate random nubers for the step over the barrier.
-   Z = randn(nbrMC_z,nbr_MC);
+ Z = randn(nbrMC_z,nbr_MC);
     
  % Obtain valued for the process and derivatives on the other side of
  % the barrier:
    
- firstpart=repmat(X(end,:)+r*h*X(end,:),nbrMC_z,1);
- lastpart=repmat(sigma*X(end,:)*sqrt(h),nbrMC_z,1);
+ firstpart = repmat(X(end,:)+r*h*X(end,:),nbrMC_z,1);
+ lastpart = repmat(sigma*X(end,:)*sqrt(h),nbrMC_z,1);
  % First derivatives:
     X_Tplus = firstpart + Z.*lastpart;
-    X_Tminus = firstpart-Z.*lastpart;
+    X_Tminus = firstpart - Z.*lastpart;
     X_Tdot = firstpart;
 
     V_Tplus = payoff(X_Tplus,K);
@@ -72,9 +72,15 @@ end
  
     % Here, apply an AD-tequnique to get 2nd order derivatives. 
 
+    
+% Here, redo simulation until 1 step before next barrier:
+X = [X;Xend];
+
+% payoff = V(end)*prodsum(I1, I2 ...) , Ii = 1 if X > K else 0, at a specific barrier checkingpoint.  
+
 %% Verification:
 
-tt=0;
+tt = 0;
 d1 = (log(Startvalue/K) + (r+0.5*sigma^2)*(T-tt))./(sigma*sqrt(T-tt));
 d2 = d1 - sigma*sqrt(T-tt);
 
@@ -83,28 +89,18 @@ vega_verification = Startvalue.*pdf('Normal',d1,0,1).*sqrt(T-tt);
 price_verification = Startvalue.*cdf('Normal',d1,0,1) - exp(-r*(T-tt)).*K.*cdf('Normal',d2,0,1);
 
 
+formatSpec = 'Optionvalue = %4.8f, Vega= %4.8f, Rho= %4.8f, Theta= %4.8f';
+fprintf(formatSpec,price_verification(1),vega_verification(1),0,0)
 
-
-delta_verification
-Delta
-vega_verification 
-Vega
-price_verification
-Price
+%delta_verification
+%Delta
+%vega_verification 
+%Vega
+%price_verification
+%Price
 %%
 %-------------------------------------------------------------------------%
 function P = payoff(x,K)
    P=max(x-K,0);
 end
 
-function P = payoff_eval(x,K)
-   P=max(x-K,0);  
-end
-
-function P = payoff_delta(x,K)
-    P = abs((x-K)>0);
-end
-
-function P = payoff_vega(x,K)
-    P = abs((x-K)>0);
-end
